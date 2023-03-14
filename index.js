52a7bf4224632dfc49c7137f9f2584a3150b19f0
Smruti Ranjan Behera <smrutiranjanbehera422@gmail.com>
2023-03-15 00:13:45 +0530

    all faculties visible at home

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 class Faculty {
-    constructor(name, details, imagePath) {
-      this.Name = name;
-      this.Details = details;
-      this.Imagepath = imagePath;
+    constructor(Name, Details, Imagepath) {
+      this.Name = Name;
+      this.Details = Details;
+      this.Imagepath = Imagepath;
+    }
+    getName(){
+      return "${this.Name}";
+    }
+    getDetails(){
+      return "${this.Details}";
+    }
+    getImagepath(){
+      return "${this.Imagepath}";
     }
   }
   
-const amiya_rath = new Faculty('Prof. Amiya Kumar Rath', 'Professor (On Deputation)', 'images/faculty_photos/amiya_rath.jpg');
-const manas_kabat = new Faculty('Prof. Manas Ranjan Kabat', 'Professor', 'images/faculty_photos/manas-ranjan-kabat_photo1648148453.jpg');
-const rakesh_mohanty = new Faculty('Dr. Rakesh Mohanty', 'Associate Professor', 'images/faculty_photos/rakesh-mohanty.jpg');
-const suvasini_panigrahi = new Faculty('Dr. Suvasini Panigrahi', 'Associate Professor & Head', 'images/faculty_photos/suvasini-panigrahi_photo1483483429.jpg');
-const satya_prkash = new Faculty('Mr. Satya Prakash Sahoo', 'Assistant Professor', 'images/faculty_photos/satya-prakash-sahoo.jpg');
-const sumitra_kisan = new Faculty('Ms. Sumitra Kisan', 'Assistant Professor', 'images/faculty_photos/sumitra-kisan1409966677.jpg');
-const santosh_majhi = new Faculty('Dr. Santosh Kumar Majhi', 'Assistant Professor', 'images/faculty_photos/santosh-kumar-majhi_photo1465444875.jpg');
-const alina_mishra = new Faculty('Ms. ALINA MISHRA', 'Assistant Professor', 'images/faculty_photos/alina-mishra1437673698.jpg');
-const santi_behera = new Faculty('Dr. (Mrs.) Santi Behera', 'Assistant Professor', 'images/faculty_photos/-mrs.santi-behera_photo1633751124.jpg');
-const alina_dash = new Faculty('Ms. Alina Dash', 'Assistant Professor', 'images/faculty_photos/alina-dash_photo1599172693.jpg');
+const amiya_rath = new Faculty("Prof. Amiya Kumar Rath", "Professor (On Deputation)", "images/faculty_photos/amiya_rath.jpg");
+const manas_kabat = new Faculty("Prof. Manas Ranjan Kabat", "Professor", "images/faculty_photos/manas-ranjan-kabat_photo1648148453.jpg");
+const rakesh_mohanty = new Faculty("Dr. Rakesh Mohanty", "Associate Professor", "images/faculty_photos/rakesh-mohanty.jpg");
+const suvasini_panigrahi = new Faculty("Dr. Suvasini Panigrahi", "Associate Professor & Head", "images/faculty_photos/suvasini-panigrahi_photo1483483429.jpg");
+const satya_prkash = new Faculty("Mr. Satya Prakash Sahoo", "Assistant Professor", "images/faculty_photos/satya-prakash-sahoo.jpg");
+const sumitra_kisan = new Faculty("Ms. Sumitra Kisan", "Assistant Professor", "images/faculty_photos/sumitra-kisan1409966677.jpg");
+const santosh_majhi = new Faculty("Dr. Santosh Kumar Majhi", "Assistant Professor", "images/faculty_photos/santosh-kumar-majhi_photo1465444875.jpg");
+const alina_mishra = new Faculty("Ms. ALINA MISHRA", "Assistant Professor", "images/faculty_photos/alina-mishra1437673698.jpg");
+const santi_behera = new Faculty("Dr. (Mrs.) Santi Behera", "Assistant Professor", "images/faculty_photos/-mrs.santi-behera_photo1633751124.jpg");
+const alina_dash = new Faculty("Ms. Alina Dash", "Assistant Professor", "images/faculty_photos/alina-dash_photo1599172693.jpg");
